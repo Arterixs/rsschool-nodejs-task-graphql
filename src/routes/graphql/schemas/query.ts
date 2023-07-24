a7defde67fb3f75ac27b904c6cae4a3ab6d94db1
Arterixs <artemontugg@gmail.com>
2023-07-25 01:01:56 +0300

    refactor: delete excess code

--- src/routes/graphql/schemas/query.ts
+++ src/routes/graphql/schemas/query.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
 import { GraphQLList, GraphQLObjectType } from 'graphql';
 import { member, memberEnum } from './memberTypes.js';
 import { UUIDType } from '../types/uuid.js';
@@ -7,7 +6,6 @@ import { userType } from './users.js';
 import { profiles } from './profiles.js';
 import { MemberTypeId } from '../../member-types/schemas.js';
 import { Context, Id } from '../types/interface.js';
-import DataLoader from 'dataloader';
 
 export const queryType = new GraphQLObjectType({
   name: 'Query',
@@ -55,29 +53,6 @@ export const queryType = new GraphQLObjectType({
       resolve: async (_source, _args, { prisma }: Context) => {
         return await prisma.user.findMany();
       },
-      // resolve: (parent, _args, context: Context, info) => {
-      //   // context.dataloaders был создан на уровне сервера (см сниппет кода выше)
-      //   const { dataLoaders, prisma } = context;
-
-      //   // единожды инициализируем DataLoader для получения авторов по ids
-      //   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-      //   let dl = dataLoaders.get(info.fieldNodes);
-      //   if (!dl) {
-      //     dl = new DataLoader(async (ids: any) => {
-      //       // обращаемся в базу чтоб получить авторов по ids
-      //       const rows = await prisma.user.findMany();
-      //       // IMPORTANT: сортируем данные из базы в том порядке, как нам передали ids
-      //       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-      //       const sortedInIdsOrder = ids.map((id) => rows.find((x) => x.id === id));
-      //       // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-      //       return sortedInIdsOrder;
-      //     });
-      //     // ложим инстанс дата-лоадера в WeakMap для повторного использования
-      //     dataLoaders.set(info.fieldNodes, dl);
-      //   }
-      //   // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access
-      //   return dl.load(1);
-      // },
     },
     user: {
       type: userType,
